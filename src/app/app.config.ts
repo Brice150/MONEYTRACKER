81b603bde197c6a48bb9388b669ba078ecf59f67
brice150 <brice.lecomte0@gmail.com>
2025-06-28 15:33:41 +0200

    welcome page start

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,16 @@ import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  FirebaseApp,
+  initializeApp,
+  provideFirebaseApp,
+} from '@angular/fire/app';
+import { getAuth, GoogleAuthProvider, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+
+const firebaseApp: FirebaseApp = initializeApp(environment.firebase);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,8 +22,12 @@ export const appConfig: ApplicationConfig = {
     provideToastr(),
     provideAnimationsAsync(),
     provideHttpClient(),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirebaseApp(() => firebaseApp),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
+    {
+      provide: GoogleAuthProvider,
+      useValue: new GoogleAuthProvider(),
+    },
   ],
 };
